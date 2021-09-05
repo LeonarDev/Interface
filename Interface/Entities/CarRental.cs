418acd8f2e37d80c68b49894e1bb0a336b97c092
LeonarDev <leonardo.majevski@gmail.com>
2021-09-05 11:54:37 -0300

    Inicia entidades e serviços para implementação da solução com interface

---
+++ Interface/Entities/CarRental.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WithoutInterface.Entities
+{
+    class CarRental
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime FinishDate { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public Invoice Invoice { get; set; }
+
+        public CarRental(DateTime startDate, DateTime finishDate, Vehicle vehicle)
+        {
+            StartDate = startDate;
+            FinishDate = finishDate;
+            Vehicle = vehicle;
+            Invoice = null;
+        }
+    }
+}
